b9989a0cc13c61a3dfac63b51d3ad415ce613a56
maureenndiema <ndiemam@gmail.com>
2022-06-22 01:22:54 +0300

    adding weather responses

--- climateapp/views.py
+++ climateapp/views.py
@@ -6,7 +6,12 @@ def index(request):
     appid ='ffef4e2d3fc816c6a280afaf80fe6d74'
     URL ='https://api.openweathermap.org/data/2.5/weather'
     PARAMS = {'q':'Nairobi','appid':appid, 'lat':'lat', 'lon':'lon', 'units':'metric' }
-    r=requests.get()
+    r=requests.get(url=URL, params=PARAMS)
+    res = r.json()
+    description = res['weather'][0]['description']
+    icon = res['weather'][0]['icon']
+    temp = res['main'][0]['temp']
+    
 
 
     return render(request, 'weather/index.html',{'num': num})
