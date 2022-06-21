7d574c521b0aa8e0b52adeff076ed55d980044e8
maureenndiema <ndiemam@gmail.com>
2022-06-22 01:12:57 +0300

    adding weather api and params

--- climateapp/views.py
+++ climateapp/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 def index(request):
-    return render(request, 'weather/index.html')
+    appid ='ffef4e2d3fc816c6a280afaf80fe6d74'
+    URL ='https://api.openweathermap.org/data/2.5/weather'
+    PARAMS = {'q':'Nairobi','appid':appid, 'lat':'lat', 'lon':'lon', 'units':'metric' }
+    r=requests.get()
+
+
+    return render(request, 'weather/index.html',{'num': num})
